8c8a38a4efe5f60c7a4b64bda43618bf0d67fa00
Cagdas_Yetkin <cagdas_yetkin@jabil.com>
2023-07-17 12:25:39 +0200

    review notes only

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import (
     HomePageView,
     SubjectPageView,
-    LearningPageView,
+    #LearningPageView,
+    LearningPageView2,
     DashboardPageView,
     AboutPageView,
 )
 
 urlpatterns = [
-    path("learning/", LearningPageView.as_view(), name="learning"),
+    path("learning/", LearningPageView2.as_view(), name="learning"),
     path("subject/", SubjectPageView.as_view(), name="subject"),
     path("dashboard/", DashboardPageView.as_view(), name="dashboard"),
     path("about/", AboutPageView.as_view(), name="about"),
--- learning_app/views.py
+++ learning_app/views.py
@@ -18,12 +18,32 @@ class SubjectPageView(CreateView):
     success_url = reverse_lazy("learning")
 
 
-class LearningPageView(CreateView):
+#class LearningPageView(CreateView):
+#    model = LearningItem
+#    template_name = "learning.html"
+#    fields = ["completed", "text"]
+#    context_object_name = "list_of_items"
+#    success_url = reverse_lazy("learning")
+
+class LearningPageView2(CreateView):
     model = LearningItem
     template_name = "learning.html"
-    fields = ["completed", "text"]
-    context_object_name = "list_of_items"
+    form_class = ItemForm
     success_url = reverse_lazy("learning")
+
+    def form_valid(self, form):
+        form.instance.student = self.request.user.student
+        return super().form_valid(form)
+
+    def get_queryset(self):
+        return LearningItem.objects.filter(student=self.request.user.student)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["queryset"] = self.get_queryset()
+        context["list_of_items"] = LearningItem.objects.filter(student=self.request.user.student)
+        return context 
+
 
 
 class DashboardPageView(ListView):
